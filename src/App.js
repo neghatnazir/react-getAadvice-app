4b0768d86d79c583244cf4a296f2ba11a34d740c
neghatnazir <neghatnazir@gmail.com>
2022-10-17 20:09:35 +0530

    react get a great advice app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+const App = () => {
+const [advice, setAdvice] = useState('');
+const [ isActive, setIsActive] = useState(false);
+
+useEffect(() => {
+  fetchAdvice()
+},[advice])
+
+ const fetchAdvice = () => {
+   axios.get('https://api.adviceslip.com/advice')
+     .then((response) => {
+       const { advice } = response.data.slip;
+
+       setAdvice(advice );
+     })
+     .catch((error) => {
+       console.log(error);
+     });
+  //  console.log('fettch')
+ } 
+ const focusButton = () => {
+   
+    setIsActive(isActive => !isActive);
+ } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className="card">
+      <h1  className="heading" >{advice}</h1>
+      <button className="button" style={{backgroundColor: isActive ? '#90EE90'  : '#ffffff'}} onMouseEnter={focusButton} onMouseLeave={focusButton} onClick={fetchAdvice} >
+        <span>GIVE ME ADVICE!</span>
+      </button>
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
+
